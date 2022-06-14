19515c0fba4c499e1e0fae1c9cdc44422dfdffdc
Konstantin Voykov <koykov0@gmail.com>
2022-06-14 23:06:50 +0300

    ZeroMQ module: implement broadcaster.

---
+++ zeromq/broadcaster.go
@@ -0,0 +1,61 @@
+package zeromq
+
+import (
+	"context"
+	"sync"
+
+	"github.com/koykov/traceID"
+	"github.com/koykov/traceID/broadcaster"
+	"github.com/pebbe/zmq4"
+)
+
+type Broadcaster struct {
+	broadcaster.Base
+	once  sync.Once
+	ctx   *zmq4.Context
+	sock  *zmq4.Socket
+	topic []byte
+	err   error
+}
+
+func (b *Broadcaster) Broadcast(_ context.Context, p []byte) (n int, err error) {
+	b.once.Do(func() {
+		conf := b.GetConfig()
+		if len(conf.Topic) == 0 {
+			conf.Topic = traceID.DefaultZeroMQTopic
+		}
+		b.topic = []byte(conf.Topic)
+
+		if b.ctx, b.err = zmq4.NewContext(); b.err != nil {
+			return
+		}
+		if b.sock, b.err = b.ctx.NewSocket(zmq4.PUB); b.err != nil {
+			return
+		}
+		if conf.HWM == 0 {
+			conf.HWM = traceID.DefaultZeroMQHWM
+		}
+		if b.err = b.sock.SetSndhwm(int(conf.HWM)); b.err != nil {
+			return
+		}
+		if b.err = b.sock.Connect(conf.Addr); b.err != nil {
+			return
+		}
+	})
+
+	if b.err != nil {
+		err = b.err
+		return
+	}
+
+	if n, err = b.sock.SendBytes(b.topic, zmq4.SNDMORE); err != nil {
+		return
+	}
+	var n1 int
+	if n1, err = b.sock.SendBytes(p, 0); err != nil {
+		return
+	}
+	n += n1
+
+	return
+}
--- zeromq/go.mod
+++ zeromq/go.mod
@@ -1,3 +1,8 @@
 module github.com/koykov/tracemod/zeromq
 
 go 1.16
+
+require (
+	github.com/koykov/traceID v0.0.0-20220614195313-ecfb2a8f595c
+	github.com/pebbe/zmq4 v1.2.9
+)
